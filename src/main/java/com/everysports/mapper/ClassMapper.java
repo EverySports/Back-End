74b1faae1bc0d64f464a735533abb2c19bdd0244
comojin1994 <comojin1994@gmail.com>
2020-07-30 17:35:15 +0900

    Add mappers

--- src/main/java/com/everysports/mapper/ClassMapper.java
+++ src/main/java/com/everysports/mapper/ClassMapper.java
@@ -1,4 +1,9 @@
 package com.everysports.mapper;
 
+import com.everysports.domain.ClassVO;
+
+import java.util.List;
+
 public interface ClassMapper {
+    public List<ClassVO> selectClass();
 }
--- src/main/java/com/everysports/mapper/NewClassMapper.java
+++ src/main/java/com/everysports/mapper/NewClassMapper.java
@@ -1,4 +1,9 @@
 package com.everysports.mapper;
 
+import com.everysports.domain.NewClassVO;
+
+import java.util.List;
+
 public interface NewClassMapper {
+    public List<NewClassVO> selectNewClass();
 }
