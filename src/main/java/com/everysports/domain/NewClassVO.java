a8d382726bb1a1823f992f23817cdf9ef67a6c0a
comojin1994 <comojin1994@gmail.com>
2020-08-03 14:34:20 +0900

    Fix NewClassVO

--- src/main/java/com/everysports/domain/NewClassVO.java
+++ src/main/java/com/everysports/domain/NewClassVO.java
@@ -2,11 +2,13 @@ package com.everysports.domain;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class NewClassVO {
     private String class_Name;
     private Long class_Price;
-    private Long class_Num;
+    private Date class_OpenDate;
     private String teacher_Name;
     private String uploadPath;
     private String fileName;
