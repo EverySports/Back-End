84cedc2e704df914fcc136303df735d49c82e664
comojin1994 <comojin1994@gmail.com>
2020-08-03 14:39:35 +0900

    Add HotClassMapper java

--- src/main/java/com/everysports/mapper/HotClassMapper.java
+++ src/main/java/com/everysports/mapper/HotClassMapper.java
@@ -1,4 +1,9 @@
 package com.everysports.mapper;
 
+import com.everysports.domain.HotClassVO;
+
+import java.util.List;
+
 public interface HotClassMapper {
+    public List<HotClassVO> selectHotClass();
 }
