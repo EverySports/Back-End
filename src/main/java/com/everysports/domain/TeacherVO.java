84d3e4911df45f4d5f401bfd07f430f31a442e8c
comojin1994 <comojin1994@gmail.com>
2020-08-04 16:52:45 +0900

    Fix TeacherVO

--- src/main/java/com/everysports/domain/TeacherVO.java
+++ src/main/java/com/everysports/domain/TeacherVO.java
@@ -2,16 +2,13 @@ package com.everysports.domain;
 
 import lombok.Data;
 
-import java.util.Date;
-
 @Data
 public class TeacherVO {
-    private Long teacher_ID;
-    private String teacher_Email;
     private String teacher_Name;
+    private String teacher_Email;
     private String teacher_Gender;
-    private Date teacher_Birthday;
-    private String teacher_Phone;
     private String teacher_Content;
-    private Long teacher_Point;
+    private Long class_SumNum;
+    private String uploadPath;
+    private String fileName;
 }
